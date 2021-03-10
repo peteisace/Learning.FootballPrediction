db14742af11be8533a616ad83302e01472f47ca9
Pete Coleman <colemanpjh@gmail.com>
2021-03-11 00:21:35 +0700

    Docker compose works

--- Learning.FootballPrediction.Api/Program.cs
+++ Learning.FootballPrediction.Api/Program.cs
@@ -14,16 +14,23 @@ namespace Learning.FootballPrediction.Api
     public class Program
     {
         public static void Main(string[] args)
-        {            
+        {       
+            var port = 4991;     
+            if(args.Length >= 1)
+            {
+                port = int.Parse(args[0]);
+            }
+
             CreateHostBuilder(args).Build().Run();
         }        
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, int port) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseWebRoot("wwwroot");   
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls($"http://*:{port}");
                 });
     }
 }
--- Learning.FootballPrediction.DataFetch/Program.cs
+++ Learning.FootballPrediction.DataFetch/Program.cs
@@ -8,24 +8,9 @@ namespace Learning.FootballPrediction.DataFetch
     class Program
     {
         private const string URI = "http://localhost:4990/v2/competitions/{0}/Matches?season={1}&matchday={2}";
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var port = -1;
-            if(args.Length != 1 || !int.TryParse(args[0], out port))
-            {
-                Console.Out.WriteLine("Usage: DataFetch [PortNumber]");
-                port = 4991; 
-            }
-
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>()
-                    .UseUrls($"http://*:{port}");
-                });
+            
+        }        
     }
 }
